56ffed8845774eaaab40e90bd6de745595904f12
Florian Grimm <schlanger@solvin.com>
2026-04-24 19:17:03 +0200

    add test

--- Brimborium.Sprache.Tests/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Brimborium.Sprache.Tests {
-    public class Class1 {
-        [Test]
-        public async Task Test001() {
-            var a = 1;
-            await Assert.That(a).IsEqualTo(1);
-        }
-    }
-}
